ca151e9d525f5bf1ec4b3c5ec02ec0898b5b21ca
井手 俊文 <knocknote.ide@gmail.com>
2021-09-18 14:40:36 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -8,4 +8,8 @@
         content="本文2"
         :message="$message"
     />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="クラスベースのメッセージです" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="クラスベースのメッセージです" defaultMessage="初期値から変更しています"/>
 </x-tests.app>
