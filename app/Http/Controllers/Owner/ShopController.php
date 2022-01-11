1ef64ab9e967fc34083fd4825555ed60c89819b3
井手 俊文 <toshifumiide198765@gmail.com>
2022-01-11 14:34:42 +0900

    リサイズなしのアップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,7 @@ use App\Models\Shop;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\SUpport\Facades\Storage;
 
 class ShopController extends Controller {
     // コントローラー側でも認証確認
@@ -15,16 +16,16 @@ class ShopController extends Controller {
     public function __construct() {
         // ユーザー認証が
         $this->middleware("auth:owners");
-        $this->middleware(function ($request, $next){
+        $this->middleware(function ($request, $next) {
             // まずは取得情報を確認
             // dd($request->route()->parameter("shop")); //文字列でid取得
             // dd(Auth::id()); //数字でid取得
             $id = $request->route()->parameter("shop"); //shopのid取得
-            if(!is_null($id)){
+            if (!is_null($id)) {
                 $shopsOwnerId = Shop::findOrFail($id)->owner->id;
                 $shopId       = (int)$shopsOwnerId; //キャスト 文字列→数字に変換
                 $ownerId      = Auth::id();
-                if($shopId !== $ownerId){ //もしshopIdとownerIdが異なっている場合
+                if ($shopId !== $ownerId) { //もしshopIdとownerIdが異なっている場合
                     abort(404); //404画面表示
                 }
             }
@@ -37,16 +38,26 @@ class ShopController extends Controller {
         // Shopモデルを使用して、取得したownerIdに該当するshopの情報を取得
         // $shops   = Shop::where("owner_id", $ownerId)->get();
         // 以上の内容は下記にまとめることも可能
-        $shops = Shop::where("owner_id" , Auth::id())->get();
+        $shops = Shop::where("owner_id", Auth::id())->get();
 
         return view("owner.shops.index", compact("shops"));
     }
     public function edit($id) {
         // 何も認証系での処理を実行していない状況で、どのIDが取得されるか確認
+        // dd(Shop::findOrFail($id));
         // 現状だと、URLにidを下手打ちすると他のownerのshop情報が取得できてしまう
-        // ミドルウェアを使用して__construct()内に
-        dd(Shop::findOrFail($id));
+        // ミドルウェアを使用して__construct()内にmiddleware(function($request , $next){})でshopの認証を実行
+        $shop = Shop::findOrFail($id);
+        return view("owner.shops.edit", compact("shop"));
     }
     public function update(Request $request, $id) {
+        // Laravel側でリサイズしないパターン（ユーザーにリサイズしてもらう場合）
+        $imageFile = $request->image;
+        // 取得したimageFileのnull判定と、念の為アップロードできているか確認を実行
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            //アップロードできている場合、imageFileをstorage/app/public/shopsに格納
+            Storage::putFile("public/shops", $imageFile);
+        }
+        return redirect()->route("owner.shops.index");
     }
 }
